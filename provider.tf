daae462979a48bf34d63856012e362509f9fc7b8
Rohit-vel <104149165+Rohit-vel@users.noreply.github.com>
2022-12-21 10:51:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = "AKIASA3APD2MX7NIYNU7"
-  secret_key = "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"
+  access_key = var.access_key
+  secret_key = var.secrete_key
 }
